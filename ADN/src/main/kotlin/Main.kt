fd78e697303d4f9ed53dba502b9468e361ba230d
PhineeTems <phineetemgoua1234@gmail.com>
2023-11-12 15:44:28 +0100

    modification des fonctions

--- ADN/src/main/kotlin/Main.kt
+++ ADN/src/main/kotlin/Main.kt
@@ -1,10 +1,6 @@
 import adn.Fibre
 import adn.Nucleotide
-import adn.Test
 
 fun main(args: Array<String>) {
-var a:Test = Test(2)
-    a.n = 8
-    println(a.n)
 
 }
--- ADN/src/main/kotlin/adn/Arn.kt
+++ ADN/src/main/kotlin/adn/Arn.kt
@@ -1,4 +1,4 @@
 package adn
 
-class Arn(Fibre) {
+class Arn() {
 }
