ccef197c215ecc59b860f5a1a4d5b46294567346
PhineeTems <phineetemgoua1234@gmail.com>
2023-11-19 11:21:12 +0100

    Re-writing all the classes in Project ADN

--- ADN/src/main/kotlin/Main.kt
+++ ADN/src/main/kotlin/Main.kt
@@ -2,5 +2,9 @@ import adn.Fibre
 import adn.Nucleotide
 
 fun main(args: Array<String>) {
+    val f:Fibre = Fibre(mutableListOf<Nucleotide>(Nucleotide.Adenine,Nucleotide.Thymine,Nucleotide.Thymine,Nucleotide.Cytosine))
 
+    println(f.nucleoTable)
+
+    println(f.duplicateFibre().nucleoTable)
 }
--- ADN/src/main/kotlin/adn/Fibre.kt
+++ ADN/src/main/kotlin/adn/Fibre.kt
@@ -16,16 +16,15 @@ class Fibre(var nucleoTable:MutableList<Nucleotide>){
         return result
     }
     fun compareFibre(f: Fibre):Boolean{
-        if (this.nucleoTable.size !== f.nucleoTable.size) {
-            return false
-        }
-        else{
-            for (i in 0 until this.nucleoTable.size){
-                if (this.nucleoTable[i] != f.nucleoTable[i].ComplementFibre())
+        if (this.nucleoTable.size == f.nucleoTable.size){
+            for (i in 0 until nucleoTable.size){
+                if (this.nucleoTable[i] != f.nucleoTable[i].ComplementFibre()){
                     return false
+                }
             }
+            return true
         }
-        return true
+        return false
     }
     fun hamming(f: Fibre): Int{
         var result: Int = 0
