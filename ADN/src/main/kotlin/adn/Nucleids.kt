c9b13e009eeea10efc057a84e0d30048128646a4
PhineeTems <phineetemgoua1234@gmail.com>
2023-11-19 12:14:52 +0100

    Re-writing all the methods in ADN Project

--- ADN/src/main/kotlin/adn/Nucleids.kt
+++ ADN/src/main/kotlin/adn/Nucleids.kt
@@ -1,8 +1,39 @@
 package adn
 
-enum class Nucleids(code: String){
-    Adenine("A"),
-    Guanine("G"),
-    Cytosine("C"),
-    Uracile("U")
+enum class Nucleids(val code: String){
+    Adenine("A"){
+        override fun ComplementFibre(): Nucleids{
+            return Nucleids.Uracile
+        }
+        override fun toString(): String {
+            return Nucleids.Adenine.code
+        }
+    },
+    Uracile("U"){
+        override fun ComplementFibre(): Nucleids {
+            return Nucleids.Adenine
+        }
+
+        override fun toString(): String {
+            return Nucleids.Uracile.code
+        }
+    },
+    Cytosine("C"){
+        override fun ComplementFibre(): Nucleids {
+            return Nucleids.Guanine
+        }
+        override fun toString(): String {
+            return Nucleids.Cytosine.code
+        }
+    },
+    Guanine("G"){
+        override fun ComplementFibre(): Nucleids {
+            return Nucleids.Cytosine
+        }
+
+        override fun toString(): String {
+            return Nucleids.Guanine.code
+        }
+    };
+    abstract fun ComplementFibre():Nucleids
 }
